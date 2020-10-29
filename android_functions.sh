8db3c14f6e1ee2820ec529192351eeedc1f4f237
Ben Hergert <bhergert@slack-corp.com>
2020-10-29 14:45:08 -0700

    Added android_functions.sh

---
+++ android_functions.sh
@@ -0,0 +1,27 @@
+# Prints the id of an emulator if one is running
+function get_emulator_id {
+  while read sn device; do
+    echo $sn
+    return
+  done < <(adb devices | sed '1d' | grep "emulator")
+}
+
+# Enables/disables Talkback on the emulator
+function talkback {
+  ID=$(get_emulator_id)
+  if [[ $1 = "on" ]]; then
+    adb -s $ID shell settings put secure enabled_accessibility_services com.google.android.marvin.talkback/com.google.android.marvin.talkback.TalkBackService
+  elif [[ $1 = "off" ]]; then
+    adb -s $ID shell settings put secure enabled_accessibility_services com.android.talkback/com.google.android.marvin.talkback.TalkBackService
+  else
+    echo "No dice!"
+    echo "Hint: Pass either 'on' or 'off' as arguments"
+  fi
+}
+
+# Checks for a non-emulator device and opens it with scrcpy
+function run_scrcpy {
+  while read sn device; do
+    [ ! -z "$sn" ] && scrcpy -s $sn
+  done < <(adb devices | sed '1d' | grep -v "emulator")
+}
